401118c1c373f16076b35fd57b0654917a07a47a
Alexandr Pirs <alexandr.pirss@mail.ru>
2016-08-15 17:36:32 +0300

    desktop v1

--- js/common.js
+++ js/common.js
@@ -4,4 +4,8 @@ $(function() {
 	$(".header .menu__fixed .menu__button i").click(function () {
 		$(".header .menu__fixed .menu__content").slideToggle("slow");
 	});
+
+	// amimate
+	// $('.intro__logo').animated('animated bounceOutLeft');
+	// $('.intro__logo').animated('animated fadeOutUp');
 });
